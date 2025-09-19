4cd37a027cdf087ea6df89d3ebf160ca74ae0c62
SreeshnaC <sreeshnacholayil07@gmail.com>
2025-09-19 15:23:03 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL.Image import preinit
+
+x = np.array([[1,0],[0,1]])
+print("Array")
+print(x)
+print("\nSum of all elements:")
+print(np.sum(x))
+print("\nSum of each column:")
+print(np.sum(x,axis=0))
+print("\nSum of each row:")
+print(np.sum(x,axis=1))
